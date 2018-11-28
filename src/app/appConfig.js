e878faa4b464df219e539891a89c5a41cd4e5cac
Levi Liester <levi.liester@gmail.com>
2018-11-28 16:12:51 -0600

    Fix sockets

--- src/app/appConfig.js
+++ src/app/appConfig.js
@@ -1,7 +1,9 @@
+const env =process.env.NODE_ENV;
+console.log(env)
 const appConfig = {
   reduxLoggerEnabled: false,
-  socketUrl: 'http://localhost:4001',
-  apiBaseUrl: process.env.NODE_ENV == 'production'? 'http://rembrandt.proxibid.com:4001':'http://localhost:4001',
+  socketUrl: process.env.NODE_ENV === "production"? 'http://rembrandt.proxibid.com:4001':'http://localhost:4001',
+  apiBaseUrl: process.env.NODE_ENV === "production"? 'http://rembrandt.proxibid.com:4001':'http://localhost:4001',
 };
 
 export default appConfig;
