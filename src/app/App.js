c45c7f371bf694dc4a88766651488babc5de292f
Levi Liester <levi.liester@gmail.com>
2018-11-28 15:53:54 -0600

    Responsive Drawer

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-
-import AppBar from './AppBar';
+import ResponsiveDrawer from './ResponsiveDrawer.js'
 
 import { storeConfig } from '../common/storeConfig';
 import theme from '../common/theme';
@@ -12,12 +11,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <MuiThemeProvider theme={ theme }>
-          <Provider store={ storeConfig }>
-            <div>
-              <AppBar/>
+        <MuiThemeProvider theme={theme}>
+          <Provider store={storeConfig}>
+            <ResponsiveDrawer>
               <AllItems/>
-            </div>
+            </ResponsiveDrawer>
           </Provider>
         </MuiThemeProvider>
       </div>
